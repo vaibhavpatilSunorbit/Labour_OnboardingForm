c6115ab3cee74528785afb3b9ff6815429291dda
vaibhavpatilSunorbit <vaibhav.pail@sunorbit.in>
2024-05-26 13:40:30 +0530

    Header, sidebar, OnBoarding form mobile responsive

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
+import OnboardingForm from './components/OnboardingForm/OnboardingForm';
 import './App.css';
 
 function App() {
+
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  const [formStatus, setFormStatus] = useState({
+    kyc: false,
+    personal: false,
+    bankDetails: false,
+    project: false
+  });
+
+  const handleFormSubmit = (formType) => {
+    setFormStatus((prevStatus) => ({
+      ...prevStatus,
+      [formType]: true
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <div className="app"> */}
+      <div className='grid-container'>
+        <Header OpenSidebar={OpenSidebar}/>
+        {/* <div className="main-content"> */}
+          <Sidebar formStatus={formStatus} openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+          <Routes>
+            <Route path="/kyc" element={<OnboardingForm formType="kyc" onFormSubmit={handleFormSubmit} />} />
+            <Route path="/personal" element={<OnboardingForm formType="personal" onFormSubmit={handleFormSubmit} />} />
+            <Route path="/project" element={<OnboardingForm formType="project" onFormSubmit={handleFormSubmit} />} />
+            <Route path="/bankDetails" element={<OnboardingForm formType="bankDetails" onFormSubmit={handleFormSubmit} />} />
+          </Routes>
+        {/* </div> */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,146 @@
+// import React, { useState } from 'react';
+// import { Link, matchRoutes } from 'react-router-dom';
+// import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+// import EditNoteIcon from '@mui/icons-material/EditNote';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+// import './sidebar.css';
+
+// function Sidebar({ formStatus }) {
+//   const [isCollapsed, setIsCollapsed] = useState(true);
+
+//   const toggleCollapse = () => {
+//     setIsCollapsed(!isCollapsed);
+//   };
+
+//   const getBulletColor = (isCompleted) => {
+//     return isCompleted ? '#20C305' : '#FFBF00';
+//   };
+
+//   return (
+//     <div className="sidebar">
+//       <div className="stages">
+//         <label>STAGES</label>
+//       </div>
+//       <div className="profile-section">
+//         <div className="profile-icon">
+//           <PersonOutlineIcon />
+//           <span className="bullet" style={{ color: getBulletColor(formStatus.kyc) }}>&#8226;</span>
+//         </div>
+//         <Link to="/kyc" className="sidebar-link">
+//           <span className="mains">KYC</span>
+//         </Link>
+//       </div>
+//       <div className="application-section">
+//         <div className="application-header" onClick={toggleCollapse}>
+//           <EditNoteIcon />         
+//           <span className="bullet" style={{ marginRight:"21px", color: getBulletColor(formStatus.personal && formStatus.bankDetails && formStatus.project) }}>&#8226;</span>
+//           <span className="mains" >Application</span>
+//           {isCollapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+//         </div>
+//         {!isCollapsed && (
+//           <ul className="application-list">
+//             <li>
+//               <Link to="/personal" className="sidebar-link">
+//                 <span className="bullet" style={{ color: getBulletColor(formStatus.personal) }}>&#8226;</span> Personal
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/bankDetails" className="sidebar-link">
+//                 <span className="bullet" style={{ color: getBulletColor(formStatus.bankDetails) }}>&#8226;</span> Bank Details
+//               </Link>
+//             </li>
+//             <li>
+//               <Link to="/project" className="sidebar-link">
+//                 <span className="bullet" style={{ color: getBulletColor(formStatus.project) }}>&#8226;</span> Project
+//               </Link>
+//             </li>
+//           </ul>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Sidebar;
+
+
+
+
+
+
+
+
+// Sidebar.js
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { AiOutlineBank } from 'react-icons/ai';
+// import { BsFillPersonPlusFill, BsFileTextFill, BsArrowRepeat, BsLockFill, BsCheckCircleFill } from 'react-icons/bs';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import VJLogo from "../../images/VJlogo-1-removebg.png"
+import './sidebar.css';
+
+function Sidebar({formStatus, openSidebarToggle, OpenSidebar }) {
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+    const toggleCollapse = () => {
+      setIsCollapsed(!isCollapsed);
+    };
+  const getBulletColor = (isCompleted) => {
+        return isCompleted ? '#20C305' : '#FFBF00';
+      };
+    
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive" : ""}>
+      <div className='sidebar-title'>
+        <div className='sidebar-brand'>
+          <img src={VJLogo} className="vjlogo" alt="logo"/>
+        </div>
+        <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+      </div>
+
+     
+         <div className="profile-icon">
+         {/* <div className="profile-section"> */}
+           <PersonOutlineIcon />
+         {/* </div> */}
+         <Link to="/kyc" className="sidebar-link">
+           <span className="bullet" style={{ color: getBulletColor(formStatus.kyc) }}>&#8226;</span>
+           <span className="mains">KYC</span>
+         </Link>
+       </div>
+       <div className="application-section">
+         <div className="application-header" onClick={toggleCollapse}>
+           <EditNoteIcon />         
+           <span className="bullet" style={{ color: getBulletColor(formStatus.personal && formStatus.bankDetails && formStatus.project) }}>&#8226;</span>
+           <span className="mains" >Application</span>
+           {isCollapsed ? <ExpandMoreIcon /> : <ExpandLessIcon />}
+         </div>
+         {!isCollapsed && (
+          <ul className="application-list">
+            <li>
+              <Link to="/personal" className="sidebar-link">
+                <span className="bullet" style={{ color: getBulletColor(formStatus.personal) }}>&#8226;</span> Personal
+              </Link>
+            </li>
+            <li>
+              <Link to="/bankDetails" className="sidebar-link">
+                <span className="bullet" style={{ color: getBulletColor(formStatus.bankDetails) }}>&#8226;</span> Bank Details
+              </Link>
+            </li>
+            <li>
+              <Link to="/project" className="sidebar-link">
+                <span className="bullet" style={{ color: getBulletColor(formStatus.project) }}>&#8226;</span> Project
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    </aside>
+  )
+}
+
+export default Sidebar;
